67a607d2c43d3301898778a3ed272940828bf01f
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2016-11-16 15:39:16 +0000

    changed background of card on index to upload phot and working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
 Equipment.destroy_all
 User.destroy_all
+
 alex = User.create(first_name:"alex", last_name:"spencer", email:"alex@me.com", password:"blabla")
-Equipment.create(title: 'Drum Kit', description: 'Yahama Drum Kit', user: alex)
+drumurl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Terry_Bozzio_drums.jpg/500px-Terry_Bozzio_drums.jpg"
+drum = Equipment.create(title: 'Drum Kit', description: 'Yahama Drum Kit', user: alex)
+drum.photo_url = drumurl
 
 bob = User.create(first_name:"bob", last_name:"fox", email:"bob@me.com", password:"blabla")
-Equipment.create(title: 'Guitar', description: 'Gibson electronic guitar', user: bob)
+guitar = Equipment.create(title: 'Guitar', description: 'Gibson electronic guitar', user: bob)
 
 josh = User.create(first_name:"josh", last_name:"wilson", email:"josh@me.com", password:"blabla")
-Equipment.create(title: 'Bass guitar', description: 'Some description', user: josh)
+bassurl = "http://sherman.ninja/resonance/wp-content/uploads/2014/10/bassguitar2.jpg"
+bass = Equipment.create(title: 'Bass guitar', description: 'Some description', user: josh)
+bass.photo_url = bassurl
 
 
 # Equipment.create(title: 'Acousting Guitar', description: 'Gibson acoustinc guitar')
