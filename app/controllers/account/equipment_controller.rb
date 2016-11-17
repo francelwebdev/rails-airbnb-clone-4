2a023b45e393fdffe02a555ebd5611476eff6aa0
Petya Nikolova <petia_182@yahoo.com>
2016-11-17 14:59:04 +0000

    map working on show page of the equipment

--- app/controllers/account/equipment_controller.rb
+++ app/controllers/account/equipment_controller.rb
@@ -5,56 +5,55 @@ class Account::EquipmentController < Account::AccountController
   def index
     @equipment = current_user.equipment.all
 
-    @equipment = Equipment.where.not(latitude: nil, longitude: nil)
+    end
 
-    @hash = Gmaps4rails.build_markers(@equipment) do |equipment, marker|
-      marker.lat flat.latitude
-      marker.lng flat.longitude
-      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-  end
+    def show
+      # @equipment = Equipment.where.not(latitude: nil, longitude: nil)
 
-  def show
-    @equipment_coordinates = { lat: @equipment.latitude, lng: @equipment.longitude }
-  end
+      @hash = Gmaps4rails.build_markers(@equipment) do |equipment, marker|
+        marker.lat equipment.latitude
+        marker.lng equipment.longitude
+      end
+    end
 
-  def new
-    @equipment = Equipment.new
-  end
+    def new
+      @equipment = Equipment.new
+    end
 
-  def create
-    @equipment = Equipment.new(equipment_params)
-    @equipment.user = current_user
-    if @equipment.save
-      redirect_to equipment_path(@equipment), notice: 'Eqiupment was successfully created.'
-    else
-      render :new
+    def create
+      @equipment = Equipment.new(equipment_params)
+      @equipment.user = current_user
+      if @equipment.save
+        redirect_to equipment_path(@equipment), notice: 'Eqiupment was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      @equipment.update(equipment_params)
+      if @equipment.save
+        redirect_to equipment_path(@equipment)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @equipment.destroy
+      redirect_to account_equipment_index_path
+    end
+
+    private
+
+    def equipment_params
+      params.require(:equipment).permit(:title, :description, :category, :price, :photo)
+    end
+
+    def find_equipment
+      @equipment = current_user.equipment.find(params[:id])
     end
   end
-
-  def edit
-  end
-
-  def update
-    @equipment.update(equipment_params)
-    if @equipment.save
-      redirect_to equipment_path(@equipment)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @equipment.destroy
-    redirect_to account_equipment_index_path
-  end
-
-  private
-
-  def equipment_params
-    params.require(:equipment).permit(:title, :description, :category, :price, :photo)
-  end
-
-  def find_equipment
-    @equipment = current_user.equipment.find(params[:id])
-  end
-end
--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -2,17 +2,21 @@ class EquipmentController < ApplicationController
 
   def index
     @equipment = Equipment.all
-    @equipment = Equipment.where.not(latitude: nil, longitude: nil)
+    # @markers = Equipment.where.not(latitude: nil, longitude: nil)
 
     @hash = Gmaps4rails.build_markers(@equipment) do |equipment, marker|
-      marker.lat flat.latitude
-      marker.lng flat.longitude
+      marker.lat equipment.latitude
+      marker.lng equipment.longitude
     end
   end
 
   def show
     @equipment = Equipment.find(params[:id])
-    @equipment_coordinates = { lat: @equipment.latitude, lng: @equipment.longitude }
+    # @equipment_coordinates = { lat: @equipment.latitude, lng: @equipment.longitude }
+        @hash = Gmaps4rails.build_markers(@equipment) do |equipment, marker|
+      marker.lat equipment.latitude
+      marker.lng equipment.longitude
+    end
   end
 
 end
